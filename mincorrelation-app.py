2f031da6721a230b7060ff998f980624549bc2d9
yaobviously <yaobviously@gmail.com>
2021-01-24 23:44:20 -0500

    Update mincorrelation-app.py

--- mincorrelation-app.py
+++ mincorrelation-app.py
@@ -24,6 +24,16 @@ def teammincorr(team):
     return df.round(1)
 
 X = teammincorr(team)
+
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    color = 'red' if val < 0 else 'black'
+    return 'color: %s' % color
+
 X = X.style.applymap(color_negative_red)
 
 st.write(X)
