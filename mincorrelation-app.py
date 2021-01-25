a4de1fe225bc900a6cc79e75e50bc42fb65285a6
yaobviously <yaobviously@gmail.com>
2021-01-25 14:58:18 -0500

    Update mincorrelation-app.py

--- mincorrelation-app.py
+++ mincorrelation-app.py
@@ -10,6 +10,8 @@ import numpy as np
 import altair as alt
 import datetime
 
+today = datetime.date.today()
+
 pd.set_option('precision', 2)
 players21 = pd.read_csv(r'https://github.com/yaobviously/minutesapp/blob/main/boxscoreappdata.csv?raw=true')
 players21 = players21.sort_values(by='Team', ascending=True)
@@ -21,9 +23,15 @@ st.write("Minutes Correlation App")
 teamlist = players21['Team'].unique().tolist()
 team = st.selectbox('Team', teamlist)
 
+d = st.date_input('Date', datetime.date('2020, 12, 22'))
+
+newp21 = players21.loc[d:today]
+playerlist = newp21.loc[newp21['Team'] == team][['Player']].unique().tolist()
+
 def teammincorr(team):
     
-    mintable = players21.loc[(players21['Team'] == team) & (players21['MPG'] >=16) & (players21['MIN'] >= 2)][['GameID', 'Player', 'MIN']]
+    mintable = players21.loc[(players21['Team'] == team) & (players21['MPG'] >=16) & (players21['MIN'] >= 2 & (players21['Player'].isin(playerlist))][['GameID', 'Player', 'MIN']]
+    
     pivottable = (mintable.pivot(index='GameID', columns='Player', values='MIN')).round(2)
     df = pivottable.corr().round(2)
     
