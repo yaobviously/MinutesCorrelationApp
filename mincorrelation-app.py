03ca528f8e6f708e27d84046431645785e4ceac7
yaobviously <yaobviously@gmail.com>
2021-01-25 15:18:01 -0500

    Update mincorrelation-app.py

--- mincorrelation-app.py
+++ mincorrelation-app.py
@@ -23,7 +23,7 @@ st.write("Minutes Correlation App")
 teamlist = players21['Team'].unique().tolist()
 team = st.selectbox('Team', teamlist)
 
-d = st.date_input('Date', datetime.date(2020, 12, 21))
+d = st.date_input('Date', datetime.date(2020, 12, 22))
 
 
 newp21 = players21.loc[d:today]
